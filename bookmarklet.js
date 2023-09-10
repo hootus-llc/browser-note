884fd7fc314dc4018c98a2eeff35794e5b956de2
hootus-llc <128255712+hootus-llc@users.noreply.github.com>
2023-09-10 09:57:51 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -36,6 +36,17 @@
     }
 
     // Function to perform an accessibility audit
+    // Function to create a link to WCAG
+    function createWCAGLink(url) {
+        const link = document.createElement('a');
+        link.href = url;
+        link.target = '_blank';
+        link.textContent = 'WCAG Guidelines';
+        link.title = 'WCAG Guidelines';
+        return link;
+    }
+
+    // Function to perform an accessibility audit
     function performAccessibilityAudit() {
         const auditResults = [];
 
@@ -45,7 +56,8 @@
             if (!image.getAttribute('alt')) {
                 auditResults.push({
                     element: image,
-                    issue: 'Image is missing alt text'
+                    issue: 'Image is missing alt text',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#images-of-text'
                 });
             }
         });
@@ -56,7 +68,8 @@
             if (!control.getAttribute('aria-label') && !control.getAttribute('aria-labelledby')) {
                 auditResults.push({
                     element: control,
-                    issue: 'Form control is missing aria-label or aria-labelledby'
+                    issue: 'Form control is missing aria-label or aria-labelledby',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#input-purposes'
                 });
             }
         });
@@ -67,7 +80,8 @@
             if (!component.getAttribute('role')) {
                 auditResults.push({
                     element: component,
-                    issue: 'Custom component is missing a role attribute'
+                    issue: 'Custom component is missing a role attribute',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#roles'
                 });
             }
         });
@@ -78,7 +92,8 @@
             if (!element.getAttribute('tabindex')) {
                 auditResults.push({
                     element: element,
-                    issue: 'Interactive element is missing a tabindex attribute'
+                    issue: 'Interactive element is missing a tabindex attribute',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#keyboard'
                 });
             }
         });
@@ -93,7 +108,8 @@
             if (calculateColorContrast(backgroundColor, color) < 4.5) {
                 auditResults.push({
                     element: element,
-                    issue: 'Low color contrast on text element'
+                    issue: 'Low color contrast on text element',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#contrast-minimum'
                 });
             }
         });
@@ -104,7 +120,8 @@
         emptyLinks.forEach(link => {
             auditResults.push({
                 element: link,
-                issue: 'Empty link (href attribute is empty)'
+                issue: 'Empty link (href attribute is empty)',
+                wcagLink: 'https://www.w3.org/TR/WCAG21/#links'
             });
         });
 
@@ -113,16 +130,106 @@
         emptyButtons.forEach(button => {
             auditResults.push({
                 element: button,
-                issue: 'Empty button'
+                issue: 'Empty button',
+                wcagLink: 'https://www.w3.org/TR/WCAG21/#buttons'
             });
         });
 
-        // Add more checks as needed
+        // Example 3: Check for missing 'alt' attributes on image maps
+        const imageMaps = document.querySelectorAll('map[name] area[href]');
+        imageMaps.forEach(area => {
+            if (!area.getAttribute('alt')) {
+                auditResults.push({
+                    element: area,
+                    issue: 'Image map area is missing alt text',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#non-text-content'
+                });
+            }
+        });
+        
+        // Example 4: Check for missing 'title' attributes on iframes
+        const iframes = document.querySelectorAll('iframe:not([title])');
+        iframes.forEach(iframe => {
+            auditResults.push({
+                element: iframe,
+                issue: 'Iframe is missing a title attribute',
+                wcagLink: 'https://www.w3.org/TR/WCAG21/#page-titled'
+            });
+        });
+        
+        // Example 5: Check for empty table headers (th elements)
+        const emptyHeaders = document.querySelectorAll('th:empty');
+        emptyHeaders.forEach(header => {
+            auditResults.push({
+                element: header,
+                issue: 'Empty table header (th element)',
+                wcagLink: 'https://www.w3.org/TR/WCAG21/#tables'
+            });
+        });
+        
+        // Example 6: Check for missing 'alt' attributes on decorative images
+        const decorativeImages = document.querySelectorAll('img[role="presentation"], img[aria-hidden="true"]');
+        decorativeImages.forEach(image => {
+            if (!image.getAttribute('alt')) {
+                auditResults.push({
+                    element: image,
+                    issue: 'Decorative image is missing alt text',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#img-element'
+                });
+            }
+        });
+        
+        // Example 7: Check for missing 'lang' attributes on language changes
+        const languageChanges = document.querySelectorAll('[lang]:not(html)');
+        languageChanges.forEach(element => {
+            if (!element.getAttribute('lang')) {
+                auditResults.push({
+                    element: element,
+                    issue: 'Language change element is missing a lang attribute',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#meaningful-sequence'
+                });
+            }
+        });
+        
+        // Example 8: Check for missing 'aria-hidden' on decorative elements
+        const decorativeElements = document.querySelectorAll('[aria-hidden="false"]');
+        decorativeElements.forEach(element => {
+            auditResults.push({
+                element: element,
+                issue: 'Decorative element should have aria-hidden="true"',
+                wcagLink: 'https://www.w3.org/TR/WCAG21/#content-structure-separation'
+            });
+        });
+        
+        // Example 9: Check for missing 'label' elements for form controls
+        const labeledFormControls = document.querySelectorAll('input:not([type="hidden"]), textarea, select');
+        labeledFormControls.forEach(control => {
+            const label = control.closest('label');
+            if (!label) {
+                auditResults.push({
+                    element: control,
+                    issue: 'Form control is missing a corresponding label element',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#label'
+                });
+            }
+        });
+        
+        // Example 10: Check for missing 'alt' attributes on image buttons
+        const imageButtons = document.querySelectorAll('input[type="image"]');
+        imageButtons.forEach(button => {
+            if (!button.getAttribute('alt')) {
+                auditResults.push({
+                    element: button,
+                    issue: 'Image button is missing alt text',
+                    wcagLink: 'https://www.w3.org/TR/WCAG21/#input-image'
+                });
+            }
+        });
 
         return auditResults;
-    }
+    };
 
-    // Function to display audit results in the DOM
+     // Function to display audit results in the DOM
     function displayAuditResults(results) {
         const auditResultsContainer = document.createElement('div');
         auditResultsContainer.id = 'audit-results';
@@ -137,7 +244,26 @@
 
         results.forEach(result => {
             const resultElement = document.createElement('div');
-            resultElement.textContent = result.issue;
+
+            const issueText = document.createElement('span');
+            issueText.textContent = result.issue;
+            issueText.style.cursor = 'pointer';
+            issueText.style.textDecoration = 'underline';
+            issueText.style.marginRight = '5px';
+            
+            const wcagLink = createWCAGLink(result.wcagLink);
+
+            resultElement.appendChild(issueText);
+            resultElement.appendChild(wcagLink);
+
+            issueText.addEventListener('mouseover', () => {
+                wcagLink.style.display = 'inline';
+            });
+
+            issueText.addEventListener('mouseout', () => {
+                wcagLink.style.display = 'none';
+            });
+
             auditResultsContainer.appendChild(resultElement);
         });
 
