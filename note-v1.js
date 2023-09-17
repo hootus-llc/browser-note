f86633e61aea9dd0d5cdd4139c7244ad916b33b7
hootus-llc <128255712+hootus-llc@users.noreply.github.com>
2023-09-17 11:14:03 -0500

    Create note-v1.js
    First commit - test.

---
+++ note-v1.js
@@ -0,0 +1,11 @@
+/*
+  Paste into bookmark url:
+  javascript:(function(){
+    var jsFileUrl = "https://hootus-llc.github.io/browser-note/note-v1.js"
+    var script = document.createElement('script'):
+    script.src = jsFileUrl;
+    document.body.appendChild(script);
+  })();
+*/
+
+console.log("initial bookmarklet setup")
