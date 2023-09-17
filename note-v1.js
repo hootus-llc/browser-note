6c532c4a1f45a8874116fdc45a0b57bbf41f58b7
hootus-llc <128255712+hootus-llc@users.noreply.github.com>
2023-09-17 11:16:45 -0500

    Update note-v1.js
    Second attempt for initial setup

--- note-v1.js
+++ note-v1.js
@@ -1,8 +1,8 @@
 /*
   Paste into bookmark url:
   javascript:(function(){
-    var jsFileUrl = "https://hootus-llc.github.io/browser-note/note-v1.js"
-    var script = document.createElement('script'):
+    var jsFileUrl = "https://hootus-llc.github.io/browser-note/note-v1.js";
+    var script = document.createElement('script');
     script.src = jsFileUrl;
     document.body.appendChild(script);
   })();
